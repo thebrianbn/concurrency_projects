eadeacc91313de6e8e5c84d3873f33c6e5695b44
thebrianbn <thebrianbn@gmail.com>
2019-04-06 16:28:03 -0400

    flop optimizations

--- cellular_automaton/part1/hw4_part1_bbn5024.cpp
+++ cellular_automaton/part1/hw4_part1_bbn5024.cpp
@@ -1,3 +1,5 @@
+#include <mpi.h>
+#include <math.h> 
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -12,7 +14,10 @@ static double get_walltime() {
 
 int main(int argc, char **argv) {
 
-    if (argc != 3) {
+    int top_index, bottom_index, left_index, right_index;
+    int taskid, numtasks;
+
+    if (argc != 4) {
         printf("%s <m> <k>\n", argv[0]);
         printf("Program for parallel Game of Life\n");
         printf("with 1D grid partitioning\n");
@@ -22,15 +27,16 @@ int main(int argc, char **argv) {
 		exit(1);
     }
 
-    unsigned long m, k;
+    unsigned long m, k, p;
 
     m = atol(argv[1]);
-
     k = atol(argv[2]);
+    p = atol(argv[3]);
 
     int *grid_current;
     int *grid_next;
     
+    // dynamic allocation for current grid
     grid_current = (int *) malloc(m * m * sizeof(int));
 	if (grid_current == NULL)
 	{
@@ -38,6 +44,7 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
+    // dynamic allocation for next grid
     grid_next = (int *) malloc(m * m * sizeof(int));
     if (grid_next == NULL)
    	{
@@ -57,6 +64,17 @@ int main(int argc, char **argv) {
         }
     }
 
+    int rows_per_worker = floor(m / p);
+    int start_row, end_row;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+
+    printf("%d", taskid);
+    
+
+
     /* initializing some cells in the middle */
     grid_current[m*m/2 + m/2 + 0] = 1;
     grid_current[m*m/2 + m/2 + 1] = 1;
@@ -72,16 +90,18 @@ int main(int argc, char **argv) {
         for (i=1; i<m-1; i++) {
             for (j=1; j<m-1; j++) {
                 /* avoiding conditionals inside inner loop */
+                top_index = (i-1)*m; bottom_index = (i+1)*m;
+                left_index = j-1; right_index = j+1;
                 int prev_state = grid_current[i*m+j];
                 int num_alive  = 
-                                grid_current[(i  )*m+j-1] + 
-                                grid_current[(i  )*m+j+1] + 
-                                grid_current[(i-1)*m+j-1] + 
-                                grid_current[(i-1)*m+j  ] + 
-                                grid_current[(i-1)*m+j+1] + 
-                                grid_current[(i+1)*m+j-1] + 
-                                grid_current[(i+1)*m+j  ] + 
-                                grid_current[(i+1)*m+j+1];
+                                grid_current[i*m+j-1] + 
+                                grid_current[i*m+j+1] + 
+                                grid_current[top_index + left_index] + 
+                                grid_current[top_index + j] + 
+                                grid_current[top_index + right_index] + 
+                                grid_current[bottom_index + left_index] + 
+                                grid_current[bottom_index + j] + 
+                                grid_current[bottom_index + right_index];
 
                 grid_next[i*m+j] = prev_state * ((num_alive == 2) + (num_alive == 3)) + (1 - prev_state) * (num_alive == 3);
             }
@@ -108,6 +128,8 @@ int main(int argc, char **argv) {
 
     /* free memory */
     free(grid_current); free(grid_next);
+
+    MPI_Finalize();
 
     return 0;
 }
