9f9c2da4ff9c78cd24b5dcd4cb80309c2048b4c6
Brian Nguyen <thebrianbn@gmail.com>
2019-04-12 19:27:20 -0400

    optimize serial

--- cellular_automaton/part1/hw4_part1_bbn5024.c
+++ cellular_automaton/part1/hw4_part1_bbn5024.c
@@ -61,17 +61,25 @@ void serial_life(int m, int k, int *board) {
     for (t=0; t<k; t++) {
         for (i=1; i<m-1; i++) {
             for (j=1; j<m-1; j++) {
+
+                // avoid repeated calculations
+                int top_index = (i-1)*m;
+                int bottom_index = (i+1)*m;
+                int center_index = i*m;
+                int left_shift = j-1;
+                int right_shift = j+1;
+
                 /* avoiding conditionals inside inner loop */
                 int prev_state = grid_current[i*m+j];
                 int num_alive  = 
-                                grid_current[(i  )*m+j-1] + 
-                                grid_current[(i  )*m+j+1] + 
-                                grid_current[(i-1)*m+j-1] + 
-                                grid_current[(i-1)*m+j  ] + 
-                                grid_current[(i-1)*m+j+1] + 
-                                grid_current[(i+1)*m+j-1] + 
-                                grid_current[(i+1)*m+j  ] + 
-                                grid_current[(i+1)*m+j+1];
+                                grid_current[center_index+left_shift] + 
+                                grid_current[center_index+right_shift] + 
+                                grid_current[top_index+left_shift] + 
+                                grid_current[top_ndex+j] + 
+                                grid_current[top_index+right_shift] + 
+                                grid_current[bottom_index+left_shift] + 
+                                grid_current[bottom_index+j  ] + 
+                                grid_current[bottom_index+right_shift];
 
                 grid_next[i*m+j] = prev_state * ((num_alive == 2) + (num_alive == 3)) + (1 - prev_state) * (num_alive == 3);
             }
@@ -215,13 +223,13 @@ int main(int argc, char **argv) {
                     prev_state = grid_current[i*m+j];
 
                     num_alive = grid_current[(i+1)*m+j-1] + 
-                                 grid_current[(i+1)*m+j  ] + 
-                                 grid_current[(i+1)*m+j+1] +
-                                 grid_current[(i  )*m+j-1] + 
-                                 grid_current[(i  )*m+j+1] + 
-                                 grid_current[(i-1)*m+j-1] + 
-                                 grid_current[(i-1)*m+j  ] + 
-                                 grid_current[(i-1)*m+j+1];
+                                grid_current[(i+1)*m+j  ] + 
+                                grid_current[(i+1)*m+j+1] +
+                                grid_current[(i  )*m+j-1] + 
+                                grid_current[(i  )*m+j+1] + 
+                                grid_current[(i-1)*m+j-1] + 
+                                grid_current[(i-1)*m+j  ] + 
+                                grid_current[(i-1)*m+j+1];
                                                
                     // update the cell in grid next to be alive or dead     
                     grid_next[i*m+j] = prev_state * ((num_alive == 2) + (num_alive == 3)) + (1 - prev_state) * (num_alive == 3);
